ffbbc3a8b18fd4c44309036cc153554afd74ad65
ShivaKranthi <maddhuri.s@northeastern.edu>
2023-04-09 23:56:10 -0400

    Uploaded the weather app assignment

---
+++ weather/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './Navbar/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { Home } from './Home/Home';
+import { HourlyWeather } from './Weather/HourlyWeather';
+import { getNextFiveDays, getNextFiveDates } from './utils/utils';
+
+function App() {
+  const nextFiveDays = getNextFiveDays();
+  const nextFiveDates = getNextFiveDates();
+
+
+  return (
+    <div className="container-fluid">
+      <Router>
+        <div>
+          <Navbar />
+          <hr />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            {
+                        nextFiveDays.map((day, index) => {
+                            return (
+                              <Route key={'route' + index} path={"/" + day} element={<HourlyWeather date={nextFiveDates[index]} />} />
+
+                            )
+                        })
+            }
+            
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather/src/Weather/CardDetail.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+
+
+export const CardDetail = (CardDetailProps) => {
+
+    console.log(process.env.REACT_APP_ICON_URL);
+
+    const url = process.env.REACT_APP_ICON_URL.concat(CardDetailProps.icon, '@4x.png');
+    console.log(url);
+
+    return(
+
+        <Card border="primary">
+            <Card.Img variant="top" src={url} />
+            <Card.Body>
+                <Card.Title>{CardDetailProps.date}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{CardDetailProps.description}</Card.Subtitle>
+                <Card.Text>
+                    <span>Highest Temperature : {CardDetailProps.highestTemp}</span>
+                    <br/>
+                    <span>Lowest Temperature :  {CardDetailProps.lowestTemp}</span>
+                    <br/>
+                    <span>Average Temperature :  {CardDetailProps.averageTemp}</span>
+                    <br/>
+                    <span>Feels like:  {CardDetailProps.feelsLike}</span>
+
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+}
